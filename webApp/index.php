1d0c3638fbd94ab4ca3a1b6f0dbedcb8c751fcb2
Giorgi Khmaladze <gkhmalad@purdue.edu>
2019-04-13 08:55:51 -0400

    Beautify server code

--- webApp/index.php
+++ webApp/index.php
@@ -1,71 +1,49 @@
 <?php
-
   include 'DatabaseConnection.php';
   //* $dbConnection = DatabaseConnection::getInstance()->getConnection();
-
   session_start();
-
   //? Variables
   $error = "";
   $password = "";
   $username = "";
   $fetchedPassword = "";
-
   //? If form was submitted
   if($_POST){
-
     //? Username field validation server-side
     if(!$_POST['username']){
-
       $error .= "A username is required to log in.<br>";
-    }
-    else{
-
+    }else{
       $username = $_POST['username'];
     }
-
     //? Password field validation server-side
     if(!$_POST['password']){
-
       $error .= "A password is required to log in.<br>";
-    }
-    else{
-
+    }else{
       $password = $_POST['password'];
     }
 
     //? Database check if validation passed
     if($error != ""){
-
       $error = '<div class="signin-error" style="color:red;"><strong>Error:</strong><br>'.$error.'</div>';
-    }
-    else{
-
+    }else{
       //? Connecting to database for credential validation
       $dbConnection = DatabaseConnection::getInstance()->getConnection();
-
       //? Querying the database with custom query
       $query = "SELECT password FROM users WHERE `username` = '".$username."'";
-
       if($result = mysqli_query($dbConnection, $query)){
-
         $row = mysqli_fetch_array($result);
         $fetchedPassword = $row['password'];
       }
-
+      //? Actual validation code
       if($fetchedPassword == $password){
-
         $_SESSION['username'] = $username;
         $_SESSION['userid'] = $row['userID'];
         header("Location: ./profile.php");
-      }
-      else{
-
+      }else{
         $error = '<div class="signin-error" style="color:red;"><strong>Error:</strong><br>Incorrect Credentials!</div>';
       }
+    }
   }
-}
-
 ?>
 
 <!DOCTYPE html>
