946dadd13e29c71b62005832327ba75f4400cd36
Mihir Somani <imihirsomani@gmail.com>
2019-04-15 04:59:17 -0400

    Code styling fix

--- webApp/DatabaseConnection.php
+++ webApp/DatabaseConnection.php
@@ -1,28 +1,21 @@
 <?php
-
 class DatabaseConnection{
-
     private static $instance = null;
     private $connection;
-
     private $host = 'localhost';
     private $username = 'root';
     private $password = '';
     private $database = 'lab6_252';
-
     private function __construct(){
         $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
     }
-
     public static function getInstance(){
         if(!self::$instance) {
             self::$instance = new DatabaseConnection();
         }
         return self::$instance;
     }
-
     public function getConnection(){
         return $this->connection;
     }
-
 }
--- webApp/index.php
+++ webApp/index.php
@@ -1,53 +1,40 @@
 <?php
-  include 'DatabaseConnection.php';
-  //* $dbConnection = DatabaseConnection::getInstance()->getConnection();
-  session_start();
-  //? Variables
-  $error = "";
-  $password = "";
-  $username = "";
-  $fetchedPassword = "";
-  //? If form was submitted
-  if($_POST){
-    //? Username field validation server-side
-    if(!$_POST['username']){
-      $error .= "A username is required to log in.<br>";
-    }else{
-      $username = $_POST['username'];
+    include 'DatabaseConnection.php';
+    $dbConnection = DatabaseConnection::getInstance()->getConnection();
+    session_start();
+    $error = "";
+    $password = "";
+    $username = "";
+    $fetchedPassword = "";
+    if($_POST){
+        if(!$_POST['username']){
+            $error .= "A username is required to log in.<br>";
+        }else{
+            $username = $_POST['username'];
+        }
+        if(!$_POST['password']){
+            $error .= "A password is required to log in.<br>";
+        }else{
+            $password = $_POST['password'];
+        }
+        if($error != ""){
+            $error = '<div class="signin-error" style="color:red;"><strong>Error:</strong><br>'.$error.'</div>';
+        }else{
+            $query = "SELECT * FROM users WHERE `username` = '".$username."'";
+            if($result = mysqli_query($dbConnection, $query)){
+                $row = mysqli_fetch_array($result);
+                $fetchedPassword = $row['password'];
+            }
+            if($fetchedPassword == $password){
+                $_SESSION['username'] = $username;
+                $_SESSION['userid'] = $row['userID'];
+                header("Location: ./profile.php");
+            }else{
+                $error = '<div class="signin-error" style="color:red;"><strong>Error:</strong><br>Incorrect Credentials!</div>';
+            }
+        }
     }
-    //? Password field validation server-side
-    if(!$_POST['password']){
-      $error .= "A password is required to log in.<br>";
-    }else{
-      $password = $_POST['password'];
-    }
-
-    //? Database check if validation passed
-    if($error != ""){
-      $error = '<div class="signin-error" style="color:red;"><strong>Error:</strong><br>'.$error.'</div>';
-    }else{
-      //? Connecting to database for credential validation
-      $dbConnection = DatabaseConnection::getInstance()->getConnection();
-      //? Querying the database with custom query
-      $query = "SELECT * FROM users WHERE `username` = '".$username."'";
-      if($result = mysqli_query($dbConnection, $query)){
-        $row = mysqli_fetch_array($result);
-        $fetchedPassword = $row['password'];
-      }
-      //? Actual validation code
-      if($fetchedPassword == $password){
-        $_SESSION['username'] = $username;
-        $_SESSION['userid'] = $row['userID'];
-        //? Closing database connection
-        mysqli_close($dbConnection);
-        header("Location: ./profile.php");
-      }else{
-        $error = '<div class="signin-error" style="color:red;"><strong>Error:</strong><br>Incorrect Credentials!</div>';
-      }
-    }
-  }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
