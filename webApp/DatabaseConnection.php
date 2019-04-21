d16553d8d6b69ec1dcf196747768a967a15fba63
gkhmalad <gkhmalad@purdue.edu>
2019-04-20 22:29:09 -0400

    Database Change

--- webApp/DatabaseConnection.php
+++ webApp/DatabaseConnection.php
@@ -2,10 +2,10 @@
 class DatabaseConnection{
     private static $instance = null;
     private $connection;
-    private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'lab6_252';
+    private $host = 'us-cdbr-iron-east-02.cleardb.net';
+    private $username = 'be0a8efb543439';
+    private $password = 'c92d1aba';
+    private $database = 'heroku_e1ba5c910aaeefe';
     private function __construct(){
         $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
     }
