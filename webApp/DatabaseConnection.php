8fc7409b92c86094624330664a48a85a60514f52
Giorgi Khmaladze <gkhmalad@purdue.edu>
2019-04-26 12:03:48 -0400

    small fix

--- webApp/DatabaseConnection.php
+++ webApp/DatabaseConnection.php
@@ -4,7 +4,7 @@ class DatabaseConnection{
     private $connection;
     private $host = 'us-cdbr-iron-east-02.cleardb.net';
     private $username = 'b57824e205c97b';
-    private $password = 'fca4188';
+    private $password = 'afca4188';
     private $database = 'heroku_50cfedd6d87cf7d';
     private function __construct(){
         $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
